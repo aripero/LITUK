25a04856af232073c92b0886689cab32aa6ccc67
Ariel Perez <ariel.lacosoft@gmail..com>
2025-12-09 07:25:28 +0000

    Fix syntax error in service-worker.js activate event

--- service-worker.js
+++ service-worker.js
@@ -53,14 +53,15 @@ self.addEventListener('fetch', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
-      return Promise.all(cacheNames.map((cacheName) => {
-        if (cacheName !== CACHE_NAME) => {
-        return Promise.all(
+      return Promise.all(
         cacheNames.map((cacheName) => {
-    return caches.delete(cacheName);
-  })
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
-});
-});
 });
 
